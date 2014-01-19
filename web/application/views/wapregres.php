f4437aeca2153cb26daf627c6645b2a2b3cbe10b
kopcomen-developer <telecom>
2014-01-19 12:33:42 +0700

    c

---
+++ web/application/views/wapregres.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Response - Subscription</title>
+		<style type="text/css">
+			body {
+				margin-left: 0px;
+				margin-top: 0px;
+				margin-right: 0px;
+				margin-bottom: 0px;
+			}
+		</style>
+<style type="text/css">
+. {color: #000000; font-family: tahoma; font-size: 10pt; font-weight: normal; font-style: normal; text-decoration: none; padding: 2px; }
+</style>
+</head>
+<body>
+<div>
+<?php echo $msg_result?>
+</div>
+</body>
+</html>
